def202211a751c856757dbd70fd7294db3460956
Timothy Arland <tcarland@gmail.com>
2009-06-17 12:52:09 -0700

    - adding client handler

---
+++ clients/tnms-console/include/ClientIOHandler.h
@@ -0,0 +1,84 @@
+#ifndef _TNMSD_CLIENTIOHANDLER_H_
+#define _TNMSD_CLIENTIOHANDLER_H_
+
+#include <set>
+
+#include "EventHandlers.hpp"
+#include "Exception.hpp"
+using namespace tcanetpp;
+
+#include "TnmsClient.h"
+using namespace tnmsCore;
+
+namespace tnmsconsole {
+
+
+typedef std::map<std::string, TnmsClient*>     ClientMap;
+
+
+class ClientIOHandler : public EventIOHandler {
+
+  public:
+
+    typedef std::map<std::string, TnmsClient*>     ClientMap;
+    typedef std::set<TnmsClient*>                  ClientSet;
+    typedef ClientMap::iterator                    iterator;
+    typedef ClientMap::const_iterator              const_iterator;
+   
+
+  public:
+
+    ClientIOHandler ( TnmsTree * tree, AuthClient * auth = NULL )
+        throw ( Exception );
+
+    virtual ~ClientIOHandler();
+
+    /*  EventIOHandler */
+
+    virtual void   handle_read    ( const EventIO * io );
+    virtual void   handle_write   ( const EventIO * io );
+    virtual void   handle_shut    ( const EventIO * io );
+    virtual void   handle_close   ( const EventIO * io );
+    virtual void   handle_destroy ( const EventIO * io );
+    
+
+    virtual bool   readable       ( const EventIO * io );
+    virtual bool   writeable      ( const EventIO * io );
+
+
+    /*  ClientIOHandler */
+
+    void           timeout        ( const EventTimer * timer );
+
+    bool           insert         ( const std::string & name,
+                                    TnmsClient * client );
+    //void           erase          ( iterator & iter );
+    //void           erase          ( iterator & begin, iterator & end );
+    void           erase          ( const std::string & name );
+
+    iterator       begin();
+    iterator       end();
+    const_iterator begin() const;
+    const_iterator end()   const;
+    size_t         size()  const;
+    void           clear();
+
+    iterator       find ( const std::string & name );
+
+  protected:
+
+    TnmsTree *               _tree;
+    AuthClient *             _auth;
+
+    ClientMap                _clients;
+    ClientSet                _clientSet;
+
+    std::string              _user;
+    std::string              _pw;
+
+};
+
+}  // namespace 
+
+#endif //  _TNMSD_CLIENTIOHANDLER_H_
+
---
+++ clients/tnms-console/src/ClientIOHandler.cpp
@@ -0,0 +1,282 @@
+#define _TNMSD_CLIENTIOHANDLER_CPP_
+
+#include "ClientIOHandler.h"
+
+#include "EventManager.h"
+#include "LogFacility.h"
+
+
+namespace tnmsconsole {
+
+
+ClientIOHandler::ClientIOHandler ( TnmsTree * tree, AuthClient * auth ) 
+    throw ( Exception )
+    : _tree(tree),
+      _auth(auth)
+{
+    if ( NULL == _tree )
+        throw Exception("ClientIOHandler() TnmsTree is NULL");
+}
+
+
+ClientIOHandler::~ClientIOHandler()
+{
+    ClientMap::iterator  cIter;
+
+    for ( cIter = _clients.begin(); cIter != _clients.end(); ++cIter ) 
+    {
+        TnmsClient * client = (TnmsClient*) cIter->second;
+
+        if ( client ) {
+            if ( client->isConnected() )
+                client->close();
+            delete cIter->second;
+        }
+    }
+    _clients.clear();
+}
+
+
+void
+ClientIOHandler::timeout ( const EventTimer * timer )
+{
+    int  rd  = 0;
+    int  sd  = 0;
+
+    ClientSet::iterator  cIter;
+
+    for ( cIter = _clientSet.begin(); cIter != _clientSet.end(); ++cIter ) {
+        TnmsClient * client = *cIter;
+        LogFacility::Message  logmsg;
+
+        logmsg << "TnmsConsoleManager::timeout (" << client->getHostStr() << ")";
+
+        if ( ! client->isConnected() || client->isConnecting() ) {
+            int c = 0;
+
+            if ( (c = client->connect()) < 0 ) {
+                logmsg << " client disconnected." << std::endl;
+                LogFacility::LogMessage(logmsg);
+                continue;
+            } else if ( c >= 0 ) {
+                logmsg << " client (re)connecting..." << std::endl;
+                LogFacility::LogMessage(logmsg);
+                timer->evmgr->addIOEvent(this, client->getDescriptor(), client);
+                continue;
+            }
+
+            if ( c > 0 ) {
+                logmsg << " client connected." << std::endl;
+                LogFacility::LogMessage(logmsg);
+            }
+        } else {
+            if ( client->isAuthorized() && ! client->isSubscribed() ) {
+                // suball?
+                ;
+            } else if ( ! client->isAuthorized() ) {
+                client->login(_user, _pw);
+            }
+        }
+
+
+        if ( (rd = client->receive()) < 0 ) {
+            client->close();
+            continue;
+        }
+
+        if ( (sd = client->send()) < 0 ) {
+            client->close();
+            continue;
+        }
+
+    }
+
+    if ( _tree )
+        _tree->updateClients();
+
+    return;
+}
+
+
+
+void
+ClientIOHandler::handle_read ( const EventIO * io )
+{
+    int  rd  = 0;
+
+    TnmsClient * client = (TnmsClient*) io->rock;
+
+    if ( (rd = client->receive()) < 0 )
+        return this->handle_close(io);
+    
+    return;
+}
+
+
+void
+ClientIOHandler::handle_write ( const EventIO * io )
+{
+    int  wt = 0;
+
+    if ( io->isServer )
+        return;
+
+    TnmsClient * client = (TnmsClient*) io->rock;
+
+    if ( (wt = client->send()) < 0 )
+        return this->handle_close(io);
+
+    return;
+}
+
+
+void
+ClientIOHandler::handle_close ( const EventIO * io )
+{
+    if ( io->isServer )
+        return;
+
+    io->evmgr->removeEvent(io->evid);
+
+    TnmsClient * client = (TnmsClient*) io->rock;
+
+    if ( client == NULL )
+        return;
+
+    ClientSet::iterator  cIter = _clientSet.find(client);
+
+    if ( cIter != _clientSet.end() ) {
+        client->close();
+    }
+
+    if ( _tree ) 
+        _tree->removeClient(client);
+    
+    return;
+}
+
+void
+ClientIOHandler::handle_shut ( const EventIO * io )
+{
+
+}
+
+void
+ClientIOHandler::handle_destroy ( const EventIO * io )
+{
+    TnmsClient * client = (TnmsClient*) io->rock;
+
+    if ( client == NULL )
+        return;
+    
+    ClientSet::iterator  cIter;
+    cIter = _clientSet.find(client);
+    if ( cIter == _clientSet.end() )
+        delete client;
+
+    return;
+}
+
+
+bool
+ClientIOHandler::readable ( const EventIO * io )
+{
+    return true;
+}
+
+
+bool
+ClientIOHandler::writeable ( const EventIO * io )
+{
+    if ( io->isServer )
+        return false;
+
+    TnmsClient * client = (TnmsClient*) io->rock;
+
+    if ( client && client->txBytesBuffered() > 0 )
+        return true;
+
+    return false;
+}
+
+
+bool
+ClientIOHandler::insert ( const std::string & name, TnmsClient * client )
+{
+    ClientMap::iterator  cIter;
+
+    cIter = _clients.find(name);
+
+    if ( cIter != _clients.end() )
+        return false;
+
+    _clients[name] = client;
+    _clientSet.insert(client);
+
+    return true;
+}
+
+void
+ClientIOHandler::erase ( const std::string & name )
+{
+    ClientMap::iterator  cIter;
+
+    cIter = _clients.find(name);
+
+    if ( cIter != _clients.end() ) {
+        cIter->second->close();
+        _clientSet.erase(cIter->second);
+        _clients.erase(cIter);
+    }
+    return;
+}
+
+ClientIOHandler::iterator
+ClientIOHandler::begin()
+{
+    return _clients.begin();
+}
+
+ClientIOHandler::iterator
+ClientIOHandler::end()
+{
+    return _clients.end();
+}
+
+ClientIOHandler::const_iterator
+ClientIOHandler::begin() const
+{
+    return _clients.begin();
+}
+
+ClientIOHandler::const_iterator
+ClientIOHandler::end() const
+{
+    return _clients.end();
+}
+
+size_t
+ClientIOHandler::size() const
+{
+    return _clients.size();
+}
+
+void
+ClientIOHandler::clear()
+{
+    _clients.clear();
+    _clientSet.clear();
+}
+
+ClientIOHandler::iterator
+ClientIOHandler::find ( const std::string & name ) 
+{
+    return(_clients.find(name));
+}
+
+
+} // namespace 
+
+
+/*  _TNMSD_CLIENTIOHANDLER_CPP_  */
+
