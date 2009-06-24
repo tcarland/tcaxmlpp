cb407f740f1f36fb73e2f478f7a4cbff79ade23a
Timothy Arland <tcarland@gmail.com>
2009-06-24 15:32:47 -0700

    - adding init scripts  - removed some stale eclipse project files

---
+++ common/tnmsCore/scripts/init.tcanms.sh
@@ -0,0 +1,378 @@
+
+SYSHOME=
+CURDIR=`dirname $0`
+
+if [ "$CURDIR" == "." ]; then
+    CURDIR=${PWD}
+fi
+
+
+if [ -n "$TCANMS_HOME" ] && [ -d $TCANMS_HOME ]; then
+    SYSHOME="$TCANMS_HOME"
+fi
+if [ -z "$SYSHOME" ] && [ -d "$TCANMS_PREFIX" ]; then
+    SYSHOME="$TCANMS_PREFIX"
+fi
+if [ -z "$SYSHOME" ]; then
+    SYSHOME="$CURDIR"
+fi
+
+
+CONFIGDIR="$SYSHOME/etc"
+
+if [ -z "$RC_TCANMS_BASHRC" ]; then
+    if [ -e $CONFIGDIR/tcanmsrc ]; then
+        echo "Using rc file from $CONFIGDIR"
+        source $CONFIGPATH/tcanmsrc
+    elif [ -e $HOME/tcanmsrc ]; then
+        echo "Using rc file from $HOME"
+        source $HOME/tcanmsrc
+    elif [ -e $CURDIR/tcanmsrc ]; then
+        echo "Using rc file from $CURDIR"
+        source $CURDIR/tcanmsrc
+    else
+        echo "Failed to locate rc file"
+        exit 1
+    fi
+fi
+
+
+BINDIR="$SYSHOME/bin"
+RUNDIR="$SYSHOME/run"
+LOGDIR="$SYSHOME/logs"
+
+export INITERRLOG="$SYSHOME/tmp/init_error.log"
+export PROCESS_STATUS_FILE="$SYSHOME/tmp/init_status.log"
+echo -n "PROCESS_STATUS_CODE=" >> $PROCESS_STATUS_FILE
+INITERR=0
+
+
+if [ -z "$RC_TCANMS_FUNCTIONS" ] && [ -e ${SYSHOME}/bin/init.tcanms_functions.sh ]; then
+    source ${SYSHOME}/bin/init.tcanms_functions.sh
+fi
+if [ -z "$RC_TCANMS_FUNCTIONS" ] && [ -e ./bin/init.tcanms_functions.sh ]; then
+    source ./bin/init.tcanms_functions.sh
+fi
+if [ -z "$RC_TCANMS_FUNCTIONS" ] && [ -e ./init.tcanms_functions.sh ]; then
+    source ./init.tcanms_functions.sh
+fi
+
+if [ -z "$RC_TCANMS_FUNCTIONS" ]; then
+    echo "Failed to locate init.tcanms_functions.sh"
+    exit 1
+fi
+
+
+
+start_services()
+{
+    local proc=$1
+    local key=$2
+    local retval=0
+
+    echo "Starting services..."
+
+    get_process_list $proc $key
+
+    retval=$?
+    if [ $retval -eq 0 ]; then
+        echo "No services found"
+        return 0
+    fi
+
+    if [ -n "$proc" ] && [ -n "$key" ]; then
+        echo "Starting service $proc for $key "
+
+        get_config_list $proc $key
+
+        retval=$?
+        if [ $retval -eq 0 ]; then
+            echo "  Config not found for $proc $key" >> $INITERRLOG
+            return 0
+        else
+            
+            start_process $proc $key $CONFIGLIST
+
+        fi
+    else
+            
+        for svc in $PROCLIST; do
+            echo "Starting processes for service: $svc"
+
+            get_config_list $svc
+            
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "  Config not found for $proc $key" >> $INITERRLOG
+                return 0
+            else
+                
+                start_all_processes $svc
+
+                retval=$?
+
+                if [ $retval -gt 0 ]; then
+                    echo "  Error starting $retval processes for $svc" >> $INITERRLOG
+                fi
+            fi
+        done
+    fi
+    
+    return 1
+}
+
+
+stop_services()
+{
+    local proc=$1
+    local key=$2
+    local retval=
+
+    echo "Stopping services..."
+
+    get_process_list $proc $key
+
+    retval=$?
+
+    if [ $retval -eq 0 ]; then
+        echo "  No services found"
+        return 0
+    fi
+
+    if [ -n "$proc" ] && [ -n "$key" ]; then
+        echo "Stopping service $proc for $key "
+
+        get_config_list $proc $key
+
+        retval=$?
+
+        if [ $retval -eq 0 ]; then
+            echo "Config not found for $proc $key"
+            return 0
+        else
+            
+            stop_process $proc $key $CONFIGLIST
+
+        fi
+    else
+            
+        for svc in $PROCLIST; do
+            echo "Stopping processes for service: $svc"
+
+            get_config_list $svc
+            
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "  Config not found for $svc"
+                return 0
+            else
+                
+                stop_all_processes $svc
+
+                retval=$?
+
+                if [ $retval -gt 0 ]; then
+                    echo "  Error stopping $retval processes for $svc" >> $INITERRLOG
+                fi
+
+            fi
+        done
+    fi
+    
+    return 1
+}
+
+
+restart_services()
+{
+    local proc=$1
+    local key=$2
+    local retval=
+    local result=0
+
+    get_process_list $proc $key
+
+    retval=$?
+
+    if [ $retval -eq 0 ]; then
+        echo "  No processes found" >> $INITERRLOG
+        return 0
+    fi
+
+    if [ -n "$proc" ] && [ -n "$key" ]; then
+        echo "Restarting service $proc for $key "
+
+        get_config_list $proc $key
+
+        retval=$?
+
+        if [ $retval -eq 0 ]; then
+            echo "  Config not found for $proc $key" >> $INITERRLOG
+            return 0
+        else
+            
+            restart_process $proc $key $CONFIGLIST
+
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "Error restarting service $proc $key" >> $INITERRLOG
+                return 0
+            fi
+        fi
+
+    else
+            
+        for svc in $PROCLIST; do
+            echo "Restarting any stopped processes for service: $svc"
+
+            get_config_list $svc
+            
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "  Config not found for $svc" >> $INITERRLOG
+                return 0
+            else
+                
+                restart_all_processes $svc
+
+                retval=$?
+
+                if [ $retval -gt 0 ]; then
+                    echo "  Restarted $retval processes for $svc" 
+                    ((result += 1))
+                fi
+            fi
+        done
+    fi
+
+    if [ $result -gt 0 ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+
+info_services()
+{
+    local proc=$1
+    local key=$2
+    local retval=
+    local result=
+
+    result=0
+
+    echo "Verifying services..."
+
+    get_process_list $proc $key
+
+    retval=$?
+
+    if [ $retval -eq 0 ]; then
+        echo "  No services found" >> $INITERRLOG
+        return 0
+    fi
+
+    if [ -n "$proc" ] && [ -n "$key" ]; then
+        echo "Checking service $proc for $key "
+
+        check_process $proc $key
+
+        retval=$?
+
+        if [ $retval -eq 0 ]; then
+	    source $PROCESS_STATUS_FILE
+            echo "  Process $proc $key:  [ ${PROCESS_STATUS_CODE} ]" >> $INITERRLOG
+	    echo "  Process $proc $key:  [ ${PROCESS_STATUS_CODE} ]"
+            result=1
+        else
+            echo "  Process $proc $key:   [ OK ]"
+        fi
+
+    else
+        for svc in $PROCLIST; do
+            echo "Checking processes for service: $svc"
+
+            get_config_list $svc
+            
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "  Configs not found for $svc" >> $INITERRLOG
+                return 0
+            else
+                
+                verify_processes $svc
+
+                retval=$?
+
+                result=`expr $result + $retval`
+            fi
+        done
+    fi
+
+    if [ $result -gt 0 ]; then
+        echo "Services not running: $result" >> $INITERRLOG
+        return 0
+    else
+        echo "All services running"
+    fi
+
+    return 1
+}
+
+
+usage()
+{
+    echo ""
+    echo "Usage: $0 {start|stop|restart|info}  [process]  [key]"
+    echo ""
+    echo "  process = process or binary name "
+    echo "  key     = process identifier "
+    echo ""
+    echo "  Note: 'restart' will only restart services determined not currently "
+    echo "  running. To force restart of a process stop|start must be used."
+    echo ""
+    return 1
+}
+
+
+retval=
+
+case "$1" in
+    'start')
+        start_services $2 $3
+        ;;
+    'stop')
+        stop_services $2 $3
+        ;;
+    'restart')
+        restart_services $2 $3
+        ;;
+    'info')
+        info_services $2 $3
+        ;;
+    *)
+        usage
+esac
+
+retval=$?
+if [ $retval -eq 0 ]; then
+    if [ -n "${EMAIL_ON_ERROR}" ] && [ ${EMAIL_ON_ERROR} == 1 ]; then
+        send_errlog $INITERRLOG
+    else
+	echo
+	echo "Error Log:"
+        cat $INITERRLOG 
+    fi
+fi
+
+rm -f $INITERRLOG
+rm -f $PROCESS_STATUS_FILE
+
+exit
+
---
+++ common/tnmsCore/scripts/init.tcanms_functions.sh
@@ -0,0 +1,481 @@
+#
+# init.tcanms_functions.sh
+
+RC_TCANMS_FUNCTIONS="true"
+PROCKEY=
+PIDFILE=
+PID=
+
+check_binary_is_script()
+{
+	local BIN=${BINDIR}/$1
+	local IS_SCRIPT=`file $BIN | sed -n '/script/p'`
+	if [ "$IS_SCRIPT" != "" ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+get_process_list()
+{
+    local PROC=$1
+    local FLIST=
+
+    if [  ! -d $CONFIGDIR ]; then
+        return 0
+    fi
+        
+    cd $CONFIGDIR
+
+    FLIST=`ls -1`
+
+    for f in $FLIST; do
+        if [ -d $f ] && [ -e "${BINDIR}/${f}" ]; then
+            if [ -n "$PROC" ]; then
+                if [ "$PROC" == "$f" ]; then
+                    PROCLIST="$PROCLIST $f"
+                fi
+            else
+                PROCLIST="$PROCLIST $f"
+            fi
+        fi
+    done
+
+    if [ -z "$PROCLIST" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+
+get_config_list()
+{
+    local PROC=$1
+    local KEY=$2
+    local LIST=
+    
+    if [  ! -d $CONFIGDIR ]; then
+        return 0
+    fi
+        
+    cd $CONFIGDIR
+
+    CONFIGLIST=""
+    LIST=`find . -name "*.xml" -print | perl -n -e 'm/.+\/(.+)\/(.+_.+xml)$/ && print "$1/$2\n"'`
+
+    for cfg in $LIST; do
+
+        if [ -n "$PROC" ]; then
+            if echo "$cfg" | grep $PROC | grep -v grep 1> /dev/null 2> /dev/null; then
+                if [ -n "$KEY" ]; then
+                    if echo "$cfg" | grep "${PROC}_${KEY}.xml" | grep -v grep 1> /dev/null 2> /dev/null; then
+                        CONFIGLIST="$CONFIGLIST $cfg"
+                    fi
+                else
+                    CONFIGLIST="$CONFIGLIST $cfg"
+                fi
+            fi
+        else
+            CONFIGLIST="$CONFIGLIST $cfg"
+        fi
+    done
+
+    if [ -z "$CONFIGLIST" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+
+check_process_pid()
+{
+    local pid=$1
+
+    if ps ax | grep $pid | grep -v grep 1> /dev/null 2> /dev/null ; then
+        return 1;
+    fi
+    echo "DEAD" >> ${PROCESS_STATUS_FILE}
+    return 0
+}
+
+
+check_process()
+{
+    local bin=$1
+    local key=$2
+    local retval=0
+
+    check_binary_is_script $bin
+    local is_script=$?
+
+    # Is the binary a script?  If so, pass the call through
+    if [ $is_script -ne 0 ]; then
+	${BINDIR}/$bin status $key
+	PID=$?  # Just set PID to 0 or 1, and return it
+	return $PID
+    fi
+
+    PIDFILE="${RUNDIR}/${bin}_${key}.pid"
+    PID=0
+
+    if [ -r $PIDFILE ]; then
+        PID=`cat $PIDFILE`
+
+        check_process_pid $PID
+
+        retval=$?
+
+        if [ $retval -eq 0 ] ; then
+            echo "DEAD" >> ${PROCESS_STATUS_FILE}
+            echo "  Removing stale pid file: $PIDFILE"
+            rm -f $PIDFILE
+            PID=0
+        fi
+    fi
+
+    return $retval
+}
+
+
+get_config_key()
+{
+    local cfg=$1
+
+    PROCKEY=`echo $cfg | perl -n -e 'm/.+\/.+_(.+)\.xml$/ && print "$1"'`
+
+    if [ -z "$PROCKEY" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+
+start_all_processes()
+{
+    local bin=$1
+    local result=0
+    local cfg=
+    local key=
+    local retval=
+
+    for cfg in $CONFIGLIST; do
+
+        get_config_key $cfg
+
+        retval=$?
+        key=$PROCKEY
+
+        if [ $retval -eq 0 ]; then
+            echo "failed to determine process key for $cfg"
+            ((result += 1))
+        else
+            
+            start_process $bin $key $cfg
+            
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "Failed to start process $bin $cfg"
+                ((result += 1))
+            fi
+        fi
+    done
+
+    return $result
+}
+
+
+start_process()
+{
+    local bin=$1
+    local key=$2
+    local cfg=$3
+    local pid=
+    local retval=0
+    local dbin=
+
+    if [ -z "$bin" ] || [ -z "$key" ]; then
+        echo "No valid process or key given"
+        return -1
+    fi
+
+    check_process $bin $key
+
+    check_binary_is_script $bin
+    local is_script=$?
+
+    # Is the binary a script?  If so, pass the call through
+    if [ $is_script -gt 0 ]; then
+	${BINDIR}/$bin start ${CONFIGDIR}/${cfg}
+	if [ $? != 1 ]; then
+		return $?
+ 	fi
+    	echo "Process $bin started"
+    else
+    	if [ $PID -gt 0 ]; then
+        	echo "Process $PID is already running..stop first"
+        	return 0
+    	fi
+
+    	if [ -n "$DEBUG" ]; then
+       	 dbin="$bin -d"
+       	 echo "'${BINDIR}/${dbin} -c ${CONFIGDIR}/${cfg} &'"
+    	else
+       	 dbin="$bin -D"
+    	fi
+    
+    	$BINDIR/$dbin -c $CONFIGDIR/$cfg 1> /dev/null 2> /dev/null &
+    	echo ""
+    	sleep 1
+
+    	pid=`ps awwwx | grep "$bin" | grep "$cfg" | grep -v "grep" | grep -v "init" | awk '{ print $1 }'`
+    	echo ""
+    
+    	echo $pid > $PIDFILE
+    	echo "Process $bin started: $pid"
+    fi
+
+    if [ $TCANMS_USE_CRON -eq 1 ]; then
+	install_cron_task $bin $key
+    fi
+    return 1
+}
+
+
+stop_all_processes()
+{
+    local bin=$1
+    local result=0
+    local cfg=
+    local key=
+    local retval=
+
+    for cfg in $CONFIGLIST; do
+
+        get_config_key $cfg
+
+        retval=$?
+        key=$PROCKEY
+
+        if [ $retval -eq 0 ]; then
+            echo "  Failed to determine process key for $cfg"
+            ((result += 1))
+        else
+            
+            stop_process $bin $key $cfg
+            
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "  Failed to properly stop process $bin $cfg"
+                ((result += 1))
+            fi
+        fi
+    done
+
+    return $result
+}
+
+
+stop_process()
+{
+    local bin=$1
+    local key=$2
+    local retval=0
+
+    if [ -z "$bin" ] || [ -z "$key" ]; then
+        echo "No valid process or key given"
+        return 0 
+    fi
+
+    check_binary_is_script $bin
+    local is_script=$?
+
+    check_process $bin $key
+    retval=$?
+
+    # Is the binary a script?  If so, pass the call through
+    if [ $is_script -gt 0 ]; then
+    	echo "Killing process"
+	${BINDIR}/$bin stop $key
+	if [ $? != 1 ]; then
+		echo "Failed to terminate process" >> $INITERRLOG
+		return 0
+	fi
+    else
+    	if [ $retval -eq 0 ]; then
+       	 	return 1
+    	fi
+
+    	echo "Killing process $PID"
+    	kill $PID
+    	sleep 3
+
+    	check_process_pid $PID
+
+    	retval=$?
+
+    	if [ $retval -gt 0 ] ; then
+       	 echo "  Sending SIGKILL"
+       	 kill -9 $PID
+    	else
+       	 echo "  Process terminated"
+    	fi
+
+    	# check again to enforce removal of pidfile
+    	check_process $bin $key
+
+    fi
+    if [ $TCANMS_USE_CRON -eq 1 ]; then
+	uninstall_cron_task $bin $key
+    fi
+    return 1
+}
+
+
+#  Will restart any processes detected as not running and will email alert
+#  any restarts to ${TCANMS_EMAIL_ADDRESS}
+#
+restart_all_processes()
+{
+    local bin=$1
+    local cfg=
+    local key=
+    local retval=
+    local result=0
+
+    for cfg in $CONFIGLIST; do
+
+        get_config_key $cfg
+
+        retval=$?
+        key=$PROCKEY
+
+        if [ $retval -eq 0 ]; then
+            echo "Failed to determine key for $bin $cfg" >> $INITERRLOG
+        else
+
+            check_process $bin $key
+
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                echo "Process for $bin $key is dead, restarting..."
+
+                start_process $bin $key $cfg
+
+                retval=$?
+
+                if [ $retval -eq 0 ]; then
+                    echo "Error starting service $bin $key" >> $INITERRLOG
+                else
+                    echo "Restarted service $bin $key " >> $INITERRLOG
+                fi
+                ((result += 1))
+            fi
+        fi
+    done
+    
+    return $result
+}
+
+
+verify_processes()
+{
+    local bin=$1
+    local cfg=
+    local key=
+    local retval=
+    local result=
+
+    result=0
+
+    for cfg in $CONFIGLIST; do
+
+        get_config_key $cfg
+
+        retval=$?
+        key=$PROCKEY
+
+        if [ $retval -eq 0 ]; then
+            echo "  Failed to determine key for $bin $cfg" >> $INITERRLOG
+        else
+            check_process $bin $key
+
+            retval=$?
+
+            if [ $retval -eq 0 ]; then
+                if [ -z "$NOEMAIL_ON_ERR" ]; then
+                    echo "  Process $bin $key :  [ DEAD ]" >> $INITERRLOG
+                fi
+                echo "  Process $bin $key :  [ DEAD ]"
+                ((result += 1))
+            else
+                echo "  Process $bin $key : $PID  [ OK ]"
+            fi
+        fi
+    done
+    
+    return $result
+}
+
+
+restart_process()
+{
+    local retval=
+    stop_process $1 $2
+    sleep 1
+    start_process $1 $2 $3
+    retval=$?
+    return $retval
+}
+
+
+send_errlog()
+{
+    local errlog=$1
+
+    if [ -e $errlog ]; then
+        mailx -s '[tcanms] init.tcanms report' ${TCANMS_EMAIL_ADDRESS} < $errlog
+    fi
+
+    return 1
+}
+
+uninstall_cron_task()
+{
+    local bin=$1
+    local key=$2
+    local bin_name=`basename $0`
+
+    echo "Uninstalling cron task for ${cfg}..."
+    local cron_file=${TCANMS_TMP}/crontab.tmp
+
+    # Be sure cfg doesn't have slashes in it
+    crontab -l | sed /"${bin_name} info ${bin} ${key}"/d > ${cron_file}
+    crontab ${cron_file}
+}
+
+install_cron_task()
+{
+    local bin=$1
+    local key=$2
+    local bin_name=`basename $0`
+    local bin_path=${TCANMS_SBIN}/${bin_name}
+
+    # First remove any previously installed cron task for this key/cfg pair
+    uninstall_cron_task ${bin} ${key}
+
+    # Now add the cron task for this key/cfg
+    # Be sure cfg doesn't have slashes in it
+    local cron_file=${TCANMS_TMP}/crontab.tmp
+    crontab -l > ${cron_file}
+    echo "*/${TCANMS_CRON_INTERVAL} * * * * export EMAIL_ON_ERROR=1 && ${bin_path} info ${bin} ${key} >> /dev/null 2>&1" >> ${cron_file}
+    echo "Installing cron task for ${bin}/${key}..."
+    crontab ${cron_file}	
+}
+
