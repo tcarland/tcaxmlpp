4211214f614b225bb03f5fcf1ae458527e047b32
Timothy Arland <tcarland@gmail.com>
2009-09-24 16:27:15 -0700

    - adding test app

---
+++ TnmsAuthd/test/soap_cl_impl.cpp
@@ -0,0 +1,105 @@
+#define _SOAP_CL_IMPL_CPP_
+
+#include <string>
+
+#include "soapH.h"
+
+
+
+//--------------------------------------------------------------------//
+// authenticate
+
+int ns1__authenticate ( soap            * s,
+                        xsd__string       username, 
+		        xsd__string       password,
+		        xsd__string       ipaddress,
+		        ns1__AuthResult & result )
+{
+    return SOAP_NO_METHOD;
+}
+
+
+int ns1__refresh ( soap         * s,
+                   xsd__string    username,
+                   xsd__string    ticket,
+                   xsd__string    ipaddress,
+                   xsd__boolean & result )
+{
+    return SOAP_NO_METHOD;
+}
+
+
+int ns1__expire ( soap         * s,
+                  xsd__string    username,
+                  xsd__string    ticket,
+                  xsd__string    ipaddress,
+                  xsd__boolean & result )
+{
+    return SOAP_NO_METHOD;
+}
+
+
+int ns1__isAuthentic ( soap         * s,
+		       xsd__string    username,
+		       xsd__string    ticket,
+		       xsd__string    ipaddress,
+		       xsd__boolean & result)
+{
+    return SOAP_NO_METHOD;
+}
+
+//--------------------------------------------------------------------//
+//  Authorization
+
+
+int ns1__authorized ( soap         * s,
+		      xsd__string    username,
+		      xsd__string    ticket,
+		      xsd__string    ipaddress,
+		      xsd__string    resourcename,
+		      xsd__boolean & result)
+{
+    return SOAP_NO_METHOD;
+}
+
+
+int ns1__getAuthorizations ( soap        * s,
+			     xsd__string   username,
+			     xsd__string   ticket,
+			     xsd__string   ipaddress,
+			     struct ns1__getAuthorizationsResponse & result)
+{
+    return SOAP_NO_METHOD;
+}
+
+
+
+//--------------------------------------------------------------------//
+//  Collectors
+
+
+int ns1__getCollectorList ( soap * s,
+                            xsd__string username,
+                            xsd__string ticket,
+                            xsd__string ipaddress,
+                            struct ns1__getCollectorListResponse & result )
+{    
+    return SOAP_NO_METHOD;
+}
+
+
+//--------------------------------------------------------------------//
+//  AuthTypes
+
+
+int ns1__getAuthTypesList ( soap * s,
+                            xsd__string username,
+                            xsd__string ticket,
+                            xsd__string ipaddress,
+                            struct ns1__getAuthTypesListResponse & result )
+{    
+    return SOAP_NO_METHOD;
+}
+
+
+
---
+++ TnmsAuthd/test/tnmsauth_test.cpp
@@ -0,0 +1,195 @@
+#define _tnmsauth_test_cpp_
+
+#include "Thread.h"
+#include "StringUtils.h"
+
+#include "soapH.h"
+#include "TnmsAuth.nsmap"
+
+#define TESTUSER  "user"
+#define TESTPASS  "myuserpass"
+
+
+//#define DEFAULT_URL      "https://localhost:2981"
+#define DEFAULT_URL      "http://localhost:15999"
+#define DEFAULT_TIMEOUT  30
+
+
+
+class TestAuthThread : public tcanetpp::Thread {
+
+  public:
+
+    TestAuthThread ( const std::string & url )
+        : Thread(true),
+          _soap(soap_new()),
+          _url(url),
+          _result(0)
+    {}
+
+    virtual ~TestAuthThread()
+    {
+        soap_done(_soap);
+        free(_soap);
+        _soap = NULL;
+    }
+
+    void run()
+    {
+        std::string  user, pw, tk, ip;
+        int          r = 0;
+
+        struct ns1__AuthResult result;
+        result.ticket  = 0;
+        result.message = 0;
+        result.success = false;
+        result.timeout = 0;
+
+        user = TESTUSER;
+        pw   = TESTPASS;
+        ip   = "1.1.1.1";
+
+        const char * action = "";
+
+        char  * userc = strdup(user.c_str());
+        char  * passc = strdup(pw.c_str());
+        char  * addrc = strdup(ip.c_str());
+
+        r = soap_call_ns1__authenticate(_soap, _url.c_str(), action,
+            userc, passc, addrc, result);
+
+        if ( r != SOAP_OK )
+        {
+            _result = 2;
+            _errStr = "Fatal error in soap call: ";
+            _errStr.append(tcanetpp::StringUtils::toString(_soap->error));
+            return;
+        }
+
+        if  ( result.success )
+            _result = 6;
+        else
+            _result = 3;
+
+        return;
+    }
+
+    int  auth_result()
+    {
+        return _result;
+    }
+
+    std::string  errorStr()
+    {
+        return this->_errStr;
+    }
+
+  private:
+    
+    struct soap *   _soap;
+    std::string     _url;
+    std::string     _errStr;
+    int             _result;
+};
+
+//------------------------------------------------------------
+
+static 
+const char TNMSAUTHTEST_VERSION [] = "0.10";
+static
+const char process [] = "auth_validate";
+
+
+
+void 
+version()
+{
+    printf("%s Version: %s\n", process, TNMSAUTHTEST_VERSION);
+    return;
+}
+
+
+void
+usage()
+{
+    printf("\nUsage: %s <timeout> [url]\n", process);
+    printf("    default url  =  '%s'\n", DEFAULT_URL);
+}
+
+
+
+/*
+ *    Return codes:
+ *
+ *    6  -  OK
+ *    5  -  Invalid Test - test did not run
+ *    4  -  Invalid Test - thread error
+ *    3  -  Failure - no data or auth failure
+ *    2  -  Failure - soap error
+ *    1  -  Failure - timeout for response
+ *   
+ */
+
+
+int main ( int argc, char **argv )
+{
+    std::string  url;
+    int          val;
+    time_t       timeout, start, now;
+
+    val   = 5;
+    now   = 0;
+    start = ::time(NULL);
+
+    if ( argc < 2 ) {
+        version();
+        usage();
+        return val;
+    }
+ 
+    if ( argc >= 2 )
+        timeout = atoi(argv[1]);
+    else
+        timeout = DEFAULT_TIMEOUT;
+
+    if ( argc == 3 )
+        url = argv[2];
+    else
+        url = DEFAULT_URL;
+   
+    TestAuthThread * auth_th = new TestAuthThread(url);
+
+    try {
+        auth_th->start();
+    } catch ( tcanetpp::ThreadException & err ) {
+        val = 4;
+        delete auth_th;
+        return val;
+    }
+
+    printf("Waiting for result\n");
+ 
+    do {
+        sleep(1);
+        now  = time(NULL);
+        val  = auth_th->auth_result();
+
+        if ( val == 6 ) {
+            printf("Success! Authentication succeeded to %s\n", url.c_str());
+            break;
+        }
+    } while ( (auth_th->isRunning()) && ((now - start) < timeout) );
+
+    printf("\n");
+
+    auth_th->stop();
+    val = auth_th->auth_result();
+
+    if ( val != 6 )
+        printf("Auth request failed: %d\n", val);
+
+    delete auth_th;
+
+    return val;
+}
+
