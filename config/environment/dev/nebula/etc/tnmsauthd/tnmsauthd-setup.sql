9bbbd2c92246d64d4fbf183d0b118ba3db379c25
Timothy Arland <tcarland@gmail.com>
2010-02-20 11:42:10 -0800

    - update dev config

--- config/environment/dev/nebula/etc/tnmsauthd/tnmsauthd-setup.sql
+++ config/environment/dev/nebula/etc/tnmsauthd/tnmsauthd-setup.sql
@@ -16,23 +16,23 @@ INSERT INTO tnmsauthd.groups (name, description)
 
 
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
-    VALUES (1, 1, "tcanms", "tcanms11b", 0, 1);
+    VALUES (1, 1, "tnms", "tnms11b", 0, 1);
 
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
-    VALUES (1, 1, "tcanms/auth/tnmsauthd-master", "tnmsauth11b", 0, 1);
+    VALUES (1, 1, "tnms/auth/tnmsauthd-master", "tnmsauth11b", 0, 1);
 
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
-    VALUES (2, 1, "tcanms/tnms-console", "tnmsconsole11b", 1, 1);
+    VALUES (2, 1, "tnms/tnms-console", "tnmsconsole11b", 1, 1);
 
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
-    VALUES (1, 1, "tcanms/tnmsd/devserver_a", "tnmsd11b", 0, 1);
+    VALUES (1, 1, "tnms/tnmsd/devserver_a", "tnmsd11b", 0, 1);
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
-    VALUES (1, 1, "tcanms/tnmsd/devserver_b", "tnmsd11b", 0, 1);
+    VALUES (1, 1, "tnms/tnmsd/devserver_b", "tnmsd11b", 0, 1);
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
-    VALUES (1, 1, "tcanms/tnmsd/devserver_c", "tnmsd11b", 0, 1);
+    VALUES (1, 1, "tnms/tnmsd/devserver_c", "tnmsd11b", 0, 1);
 
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
-    VALUES (1, 1, "tcanms/tnmsarchiver/tcanms", "tnmsarchive11b", 0, 1);
+    VALUES (1, 1, "tnms/tnmsarchiver/tnms", "tnmsarchive11b", 0, 1);
 
 INSERT INTO tnmsauthd.users ( gid, authtype_id, username, password, is_agent, internal )
     VALUES (1, 1, "tnms-console", "tnmsconsole11b", 0, 1);
