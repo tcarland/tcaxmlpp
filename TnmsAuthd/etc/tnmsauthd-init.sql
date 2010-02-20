731eda658ddc76606fd73f77747550be4c77c299
Timothy Arland <tcarland@gmail.com>
2010-02-20 11:24:56 -0800

    sync

--- TnmsAuthd/etc/tnmsauthd-init.sql
+++ TnmsAuthd/etc/tnmsauthd-init.sql
@@ -3,10 +3,9 @@ CREATE SCHEMA IF NOT EXISTS `tnmsauth`;
 
 
 CREATE USER 'tnmsauth'@'localhost' IDENTIFIED BY 'tnmsauth11b';
-GRANT ALL PRIVILEGES ON *.* TO 'tnmsauth'@'localhost';
+GRANT ALL PRIVILEGES ON *.tnmsauth TO 'tnmsauth'@'localhost';
 
 CREATE USER 'tnmsauth'@'%' IDENTIFIED BY 'tnmsauth11b';
-GRANT ALL PRIVILEGES ON *.* TO 'tnmsauth'@'%';
-
+GRANT ALL PRIVILEGES ON *.tnmsauth TO 'tnmsauth'@'%';
 
 
