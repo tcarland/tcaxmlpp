d61a410f562e1afa1f5f3c4b1392d593f073a02a
Timothy Arland <tcarland@gmail.com>
2014-10-14 12:41:07 -0700

    - sync version bump and project file updates

--- src/XmlDocument.cpp
+++ src/XmlDocument.cpp
@@ -38,7 +38,7 @@ bool
 XmlDocument::_Libinit = false;
 
 const char*
-XmlDocument::_Version = "1.4.0";
+XmlDocument::_Version = "1.4.1";
 
 
 //-------------------------------------------------------------//
