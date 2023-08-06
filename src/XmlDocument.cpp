a5c4d2458325dbe2c2de41a0851d3e9c1fedbb68
Timothy C. Arland <tcarland@gmail.com>
2023-08-05 21:07:52 -0600

    update build to lates tcamake

--- src/XmlDocument.cpp
+++ src/XmlDocument.cpp
@@ -36,7 +36,7 @@ bool
 XmlDocument::_Libinit = false;
 
 const char*
-XmlDocument::_Version = "1.1.1";
+XmlDocument::_Version = "1.2.0";
 
 
 //-------------------------------------------------------------//
