7c79c177cdb177128eaf992be5eb1050a2f8f269
Timothy C. Arland <tcarland@users.noreply.github.com>
2025-03-21 05:56:50 -0600

    sync versions tags; minor cleanup (#14)
    * sync versions tags; minor cleanup

--- src/XmlDocument.cpp
+++ src/XmlDocument.cpp
@@ -39,7 +39,7 @@ bool
 XmlDocument::_Libinit = false;
 
 const char*
-XmlDocument::_Version = "1.3.2";
+XmlDocument::_Version = "1.3.3";
 
 
 //-------------------------------------------------------------//
